1dc7535702c96477f0c8891a30ba7468fe98e74c
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-21 20:05:28 +0900

    Etc: 프로젝트 세팅

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application } from 'express';
+import router from './routes';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import { normalize } from 'path';
+
+const app: Application = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  process.env.NODE_ENV === 'production' ? logger('combined') : logger('dev'),
+);
+app.use(cookieParser());
+// Express variables
+app.set('port', normalize(process.env.PORT || '3000'));
+
+// Routing
+app.use(router);
+
+// Start server
+app.listen(app.get('port'), () => {
+  console.log(
+    `!!!App is running at http://localhost:${app.get('port')} in ${app.get(
+      'env',
+    )} mode!!!`,
+  );
+});
