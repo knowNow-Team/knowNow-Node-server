725bc4a7b5fc69395b2623d41e585d58fdb01114
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-23 15:28:35 +0900

    Style: class 기반 코드로 세팅 변경

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,45 @@
-import express, { Application } from 'express';
+import express from 'express';
 import router from './routes';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
 import { normalize } from 'path';
 import 'dotenv';
+import morgan from 'morgan';
 import connect from './configs/dbConnection';
 
-const app: Application = express();
+class Application {
+  app: express.Application;
 
-// db connect
-connect();
+  constructor() {
+    this.app = express();
+    this.settings();
+    this.middlewares();
+    this.routes();
+  }
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  process.env.NODE_ENV === 'production' ? logger('combined') : logger('dev'),
-);
-app.use(cookieParser());
+  settings() {
+    this.app.set('port', normalize(process.env.PORT || '3000'));
+  }
 
-// Express variables
-app.set('port', normalize(process.env.PORT || '3000'));
+  middlewares() {
+    this.app.use(morgan('dev'));
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+    this.app.use(cookieParser());
+    this.app.use(cors());
+    this.app.use(process.env.NODE_ENV === 'production' ? logger('combined') : logger('dev'));
+  }
 
-// Routing
-app.use(router);
+  routes() {
+    this.app.use(router);
+  }
 
-// Start server
-app.listen(app.get('port'), () => {
-  console.log(
-    `!!!App is running at http://localhost:${app.get('port')} in ${app.get(
-      'env',
-    )} mode!!!`,
-  );
-});
+  start(): void {
+    this.app.listen(this.app.get('port'), () => {
+      console.log(`!!!App is running at http://localhost:${this.app.get('port')} in ${this.app.get('env')} mode!!!`);
+    });
+  }
+}
+
+export default Application;
