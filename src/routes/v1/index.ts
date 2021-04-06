185c9283cc132300e460866f134b3ecd2eb1ed13
YeongWoooo <rdd9223@gmail.com>
2021-04-06 15:31:06 +0900

    Style: 라우팅 방식 클래스로 변경

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,6 +1,13 @@
-// /api/v1 라우팅
+// /v1 라우팅
 import express, { Router } from 'express';
+import TestRouter from './tests.route';
 
-const router: Router = express.Router();
+class V1Routes {
+  public router: Router = Router();
 
-export default router;
+  constructor() {
+    this.router.use('/tests', new TestRouter().router);
+  }
+}
+
+export default V1Routes;
