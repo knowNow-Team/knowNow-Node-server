35d64a94998ac93ce1fd7856f4aaf91c57a3dd62
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-09 02:06:16 +0900

    Feat: 필터링된 단어 추출

--- src/controllers/v1/words.controller.ts
+++ src/controllers/v1/words.controller.ts
@@ -44,3 +44,20 @@ export async function deleteWord(req: Request, res: Response) {
     res.status(500).json(`Error while delete word (${err.message})`);
   }
 }
+
+export async function getFilterWord(req: Request, res: Response) {
+  try {
+    const query = req.query;
+    // wordBookId는 추후 추가
+    const order: string = String(query.order); // 정렬(ex. 오름차순)
+    const filter: string = String(query.filter);
+    if (order && filter) {
+      const word = await wordService.getFilterWord(order, filter);
+      res.status(200).json({ data: word });
+    } else {
+      throw Error('no query');
+    }
+  } catch (err) {
+    res.status(500).json(`Error while get words (${err.message})`);
+  }
+}
