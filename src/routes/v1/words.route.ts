403fa02bde9b27be0c61de9f84f122c6bd5afd8f
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-11 00:10:34 +0900

    Etc: word 라우터 추가

---
+++ src/routes/v1/words.route.ts
@@ -0,0 +1,11 @@
+import express, { Router } from 'express';
+import * as wordController from '../../controllers/v1/words.controller';
+
+const wordRouter: Router = express.Router();
+
+wordRouter.post('/', wordController.addWord);
+wordRouter.get('/:wordId', wordController.getIndividualWord);
+wordRouter.get('/', wordController.getFilterWord);
+wordRouter.delete('/:wordId', wordController.deleteWord);
+
+export default wordRouter;
