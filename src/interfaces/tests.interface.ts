a9f70f3a91693454e0460c2fee12c3c618b71471
YeongWoooo <rdd9223@gmail.com>
2021-04-12 00:45:26 +0900

    Style: Enum타입 변경

--- src/interfaces/tests.interface.ts
+++ src/interfaces/tests.interface.ts
@@ -1,19 +1,19 @@
 export interface IWord {
-  word: {
-    wordId: string; // 영어단어 id
-    isCorrect: boolean; // 정답 여부
-    answer?: string; // 제출한 정답
-  };
+  wordId: string; // 영어단어 id
+  isCorrect: boolean; // 정답 여부
+  answer?: string; // 제출한 정답
 }
 
-export interface IStatus {
-  status: 'memorized' | 'confused' | 'doNotKnow';
+export enum ETestStatus {
+  memorized = 'memorized', // 외웠어요
+  confused = 'confused', // 헷갈려요
+  doNotKnow = 'doNotKnow', // 몰라요
 }
 
 export interface ITest {
   testerId: string; // 시험친 유저
   difficulty: string; // 난이도
-  status: IStatus[]; // 몰라요, 헷갈려요, 다외웠어요 타입
+  status: ETestStatus[]; // 몰라요, 헷갈려요, 다외웠어요 타입
   words: IWord[]; // 영어단어 리스트
   wordbooks: string[]; // 영어단어장 리스트
   score: number; // 점수
