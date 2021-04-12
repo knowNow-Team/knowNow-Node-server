2ac98ae66ac985b5fa2a64d38d732507207c4818
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-12 09:45:36 +0900

    Feat: 단어장 생성

---
+++ src/services/v1/wordBooks.service.ts
@@ -0,0 +1,10 @@
+import WordBooks, { Title, Owner, IWordBook } from '../../models/wordBooks.model';
+
+export async function addWordBook(title: Title, owner: Owner) {
+  const wordBook: IWordBook = new WordBooks();
+  wordBook.title = title;
+  wordBook.owner = owner;
+  await wordBook.save();
+  const wordBooks = await WordBooks.find({});
+  return wordBooks;
+}
