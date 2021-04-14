b82fd1cde169764a6c05cfa18fb04d295762f7ff
YeongWoooo <rdd9223@gmail.com>
2021-04-15 01:59:28 +0900

    Refactor: 본인 유효성 검사 실시 후 로직 실

--- src/controllers/v1/tests.controller.ts
+++ src/controllers/v1/tests.controller.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { TestDto } from '../../dtos/tests.dto';
 import { ITest } from '../../interfaces/tests.interface';
 import TestService from '../../services/v1/tests.service';
-import { resMessage, statusCode } from '../../utils';
+import { resMessage, statusCode, util } from '../../utils';
+import HttpException from '../../exceptions/HttpException';
 
 const TEST = '시험';
 
@@ -10,9 +11,11 @@ class TestController {
   public TestService = new TestService();
 
   public getTests = async (req: Request, res: Response, next: NextFunction) => {
+    const { testerId }: { testerId: number } = req.body; // 추후 토큰으로 받으면 유효성 검사해서 불러올 것.
     try {
-      const findAllTestsData: ITest[] = await this.TestService.findAllTest();
-      res.status(statusCode.OK).json({ message: resMessage.X_READ_ALL_SUCCESS(TEST), data: findAllTestsData });
+      const findAllTestsData: ITest[] = await this.TestService.findAllTest(testerId);
+
+      return res.status(statusCode.OK).json({ message: resMessage.X_READ_ALL_SUCCESS(TEST), data: findAllTestsData });
     } catch (error) {
       next(error);
     }
@@ -20,10 +23,12 @@ class TestController {
 
   public getTestById = async (req: Request, res: Response, next: NextFunction) => {
     const testId: string = req.params.id;
+    const { testerId }: { testerId: number } = req.body; // 추후 토큰으로 받으면 유효성 검사해서 불러올 것.
 
     try {
-      const findOneTestData: ITest = await this.TestService.findTestById(testId);
-      res.status(statusCode.OK).json({ message: resMessage.X_READ_SUCCESS(TEST), data: findOneTestData });
+      const findOneTestData: ITest = await this.TestService.findTestById(testId, testerId);
+
+      return res.status(statusCode.OK).json({ message: resMessage.X_READ_SUCCESS(TEST), data: findOneTestData });
     } catch (error) {
       next(error);
     }
@@ -34,8 +39,10 @@ class TestController {
 
     try {
       if (util.isEmpty(testData)) throw new HttpException(statusCode.BAD_REQUEST, resMessage.NULL_VALUE);
+
       const createTestData: ITest = await this.TestService.createTest(testData);
-      res.status(statusCode.CREATED).json({ message: resMessage.X_CREATE_SUCCESS(TEST), data: createTestData });
+
+      return res.status(statusCode.CREATED).json({ message: resMessage.X_CREATE_SUCCESS(TEST), data: createTestData });
     } catch (error) {
       next(error);
     }
@@ -43,12 +50,14 @@ class TestController {
 
   public updateTest = async (req: Request, res: Response, next: NextFunction) => {
     const testId: string = req.params.id;
-    const testData: ITest = req.body;
+    const testData: ITest = req.body; // 추후 토큰으로 받으면 유효성 검사해서 업데이트 할 것
 
     try {
       if (util.isEmpty(testData)) throw new HttpException(statusCode.BAD_REQUEST, resMessage.NULL_VALUE);
+
       const updateTestData: ITest = await this.TestService.updateTest(testId, testData);
-      res.status(statusCode.OK).json({ message: resMessage.X_UPDATE_SUCCESS(TEST), data: updateTestData });
+
+      return res.status(statusCode.OK).json({ message: resMessage.X_UPDATE_SUCCESS(TEST), data: updateTestData });
     } catch (error) {
       next(error);
     }
@@ -56,10 +65,12 @@ class TestController {
 
   public deleteTest = async (req: Request, res: Response, next: NextFunction) => {
     const testId: string = req.params.id;
+    const { testerId }: { testerId: number } = req.body; // 추후 토큰으로 받으면 유효성 검사해서 불러올 것.
 
     try {
       const deleteTestData: ITest = await this.TestService.deleteTestData(testId);
-      res.status(statusCode.OK).json({ message: resMessage.X_DELETE_SUCCESS(TEST), data: deleteTestData });
+
+      return res.status(statusCode.OK).json({ message: resMessage.X_DELETE_SUCCESS(TEST), data: deleteTestData });
     } catch (error) {
       next(error);
     }
