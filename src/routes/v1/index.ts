f3090bbebd295255abde76d9a5953b408d0c4bb7
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-16 15:23:06 +0900

    Etc: word 라우터 추가

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,11 @@
 // /api/v1 라우팅
 import express, { Router } from 'express';
 import wordBookRouter from './wordBooks.route';
+import wordRouter from './words.route';
 
 const apiV1Router: Router = express.Router();
 
 apiV1Router.use('/wordbooks', wordBookRouter);
+apiV1Router.use('/words', wordRouter);
 
 export default apiV1Router;
