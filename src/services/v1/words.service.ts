ead29313977c28a0053dafd587af378629afe62f
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-17 00:09:07 +0900

    Etc: word create 코드 제거

--- src/services/v1/words.service.ts
+++ src/services/v1/words.service.ts
@@ -1,23 +1,6 @@
 import { Word, WordClass, Filter, PronounceVoicePath, Meaning, IWord } from '../../models/words.model';
 import Words from '../../models/words.model';
 
-export async function addWord(
-  word: Word,
-  meaning: Meaning,
-  worldClass: WordClass,
-  filter: Filter,
-  pronounceVoicePath: PronounceVoicePath,
-) {
-  const words: IWord = new Words();
-  words.word = word;
-  words.meaning = meaning;
-  words.wordClass = worldClass;
-  words.filter = filter;
-  words.pronounceVoicePath = pronounceVoicePath;
-  await words.save();
-  return words;
-}
-
 export async function getIndividualWord(wordId: string) {
   const individualWord = await Words.findOne({
     _id: wordId,
