4afec09cc45dfc4743c22fe993ce7d56902b8f77
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-17 00:10:56 +0900

    Etc: word create 코드 제거

--- src/controllers/v1/words.controller.ts
+++ src/controllers/v1/words.controller.ts
@@ -2,21 +2,6 @@ import { Request, Response } from 'express';
 import * as wordService from '../../services/v1/words.service';
 import { Word, WordClass, PronounceVoicePath, Meaning, Filter } from '../../models/words.model';
 
-export async function addWord(req: Request, res: Response) {
-  try {
-    const body = req.body;
-    const word: Word = body.word;
-    const meaning: Meaning = body.meaning;
-    const wordClass: WordClass = body.wordClass;
-    const filter: Filter = body.filter;
-    const pronounceVoicePath: PronounceVoicePath = body.pronounceVoicePath;
-    const words = await wordService.addWord(word, meaning, wordClass, filter, pronounceVoicePath);
-    res.status(200).json({ data: words });
-  } catch (err) {
-    res.status(500).json(`Error while add word (${err.message})`);
-  }
-}
-
 export async function getIndividualWord(req: Request, res: Response) {
   try {
     const wordId: string = req.params.wordId;
