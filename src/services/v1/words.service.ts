c8cb0baa63e0d6996560b24c51f072767f4c7230
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-17 00:16:17 +0900

    Refactor: 단어 API 객체 지향 방식

--- src/services/v1/words.service.ts
+++ src/services/v1/words.service.ts
@@ -1,40 +1,34 @@
-import { Word, WordClass, Filter, PronounceVoicePath, Meaning, IWord } from '../../models/words.model';
-import Words from '../../models/words.model';
+import HttpException from '../../exceptions/HttpException';
+import { resMessage, statusCode } from '../../utils';
+import WordModel from '../../models/words.model';
+import { WordDto } from '../../dtos/words.dto';
+import { IWord } from '../../interfaces/words.interface';
 
-export async function getIndividualWord(wordId: string) {
-  const individualWord = await Words.findOne({
-    _id: wordId,
-  });
-  if (individualWord) {
-    return individualWord;
-  } else {
-    throw Error('no word');
+const WORD = '단어';
+
+class WordService {
+  public WordModel = new WordModel().getModel();
+
+  public async findWordById(wordId: string): Promise<IWord> {
+    const findWord = await this.WordModel.findOne({ _id: wordId });
+    if (!findWord) throw new HttpException(statusCode.NOT_FOUND, resMessage.NO_X(WORD));
+
+    return findWord;
+  }
+
+  public async updateWord(wordId: string, wordData: IWord): Promise<IWord> {
+    const updateWordById = await this.WordModel.findByIdAndUpdate(wordId, { ...wordData });
+    if (!updateWordById) throw new HttpException(statusCode.NOT_FOUND, resMessage.NO_X(WORD));
+
+    return updateWordById;
+  }
+
+  public async deleteWordData(wordId: string): Promise<IWord> {
+    const deleteWordById = await this.WordModel.findByIdAndDelete(wordId);
+    if (!deleteWordById) throw new HttpException(statusCode.NOT_FOUND, resMessage.NO_X(WORD));
+
+    return deleteWordById;
   }
 }
 
-export async function deleteWord(wordId: string) {
-  const deleteWord = await Words.findOne({
-    _id: wordId,
-  });
-  await Words.deleteOne({
-    _id: wordId,
-  });
-  return deleteWord;
-}
-
-export async function getFilterWord(order: string, filter: any) {
-  const filterWords = Words.find({ filter: filter });
-  if (order === 'ASC') {
-    await filterWords.sort({ word: 1 });
-  } else if (order === 'DESC') {
-    await filterWords.sort({ word: -1 });
-  } else if (order === 'NEWEST') {
-    await filterWords.sort({ createdAt: -1 });
-  } else if (order === 'RANDOM') {
-    const randomWords = await Words.aggregate([{ $sample: { size: Words.length } }, { $match: { filter: filter } }]);
-    return randomWords;
-  } else if (order === 'NEWEST') {
-    await filterWords.sort({ createdAt: -1 });
-  }
-  return filterWords;
-}
+export default WordService;
