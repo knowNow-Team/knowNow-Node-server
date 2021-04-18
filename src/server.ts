321e551160602c9a042a66fac531468e334a4628
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-19 00:14:31 +0900

    Etc: 단어, 단어장 라우트 생성

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import 'dotenv/config';
 import { validateEnv } from './utils';
 import IndexRoute from './routes/index.route';
 import TestsRoute from './routes/v1/tests.route';
+import WordBooksRoute from './routes/v1/wordBooks.route';
+import WordsRoute from './routes/v1/words.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new TestsRoute()]);
+const app = new App([new IndexRoute(), new TestsRoute(), new WordBooksRoute(), new WordsRoute()]);
 
 app.start();
