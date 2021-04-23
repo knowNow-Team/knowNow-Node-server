0a8dbe21e903c74c3fd39dc43f65a5247887ec84
YeongWoooo <rdd9223@gmail.com>
2021-04-23 11:15:13 +0900

    Env: Mysql 정보 추가

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -6,6 +6,11 @@ const validateEnv = (): void => {
     MONGO_PWD: str(),
     MONGO_IP: str(),
     MONGO_DB: str(),
+    MYSQL_HOST: str(),
+    MYSQL_USER: str(),
+    MYSQL_PWD: str(),
+    MYSQL_DB: str(),
+    JWT_SECRET: str(),
   });
 };
 
