9cc6d2e5c7e2c2f7f463e9d2d388827fb4d2de1a
YeongWoooo <rdd9223@gmail.com>
2021-04-23 11:49:26 +0900

    Env: Mysql 연동 마무리

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -13,11 +13,11 @@ export default class MongoDBConnection {
     this.connect();
     // 몽구스 커넥션에 이벤트 리스너, 에러 발생 시 에러 내용 기록, 연결 종료 시 재연결 시도
     mongoose.connection.on('error', (error) => {
-      global._logger.error('몽고디비 연결 에러', error);
+      global._logger.error('MongoDB connect error', error);
     });
 
     mongoose.connection.on('disconnected', () => {
-      global._logger.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
+      global._logger.error('MongoDB disconnected. Retry the connection.');
       this.connect();
     });
   }
@@ -31,7 +31,7 @@ export default class MongoDBConnection {
         useCreateIndex: true,
         useUnifiedTopology: true,
       })
-      .then(() => global._logger.info('몽고디비 연결 성공'))
-      .catch((error: Error) => global._logger.error('몽고디비 연결 에러', error?.message));
+      .then(() => global._logger.info('MongoDB connect success'))
+      .catch((error: Error) => global._logger.error('MongoDB connect error', error?.message));
   };
 }
--- src/configs/mysql.config.ts
+++ src/configs/mysql.config.ts
@@ -1,6 +1,6 @@
 // import config from 'config';
 import path from 'path';
-import { ConnectionOptions } from 'typeorm';
+import { ConnectionOptions, createConnection } from 'typeorm';
 import { dbConfig } from '@interfaces/db.interface';
 
 // const { host, user, password, database }: dbConfig = config.get('dbConfig');
@@ -19,23 +19,33 @@ const { host, user, password, database }: dbConfig =
         database: 'knownow',
       };
 
-const MysqlDBConnection: ConnectionOptions = {
-  type: 'mysql',
-  host: host,
-  port: 3306,
-  username: user,
-  password: password,
-  database: database,
-  synchronize: true,
-  logging: false,
-  entities: [path.join(__dirname, '../**/*.entity{.ts,.js}')],
-  migrations: [path.join(__dirname, '../**/*.migration{.ts,.js}')],
-  subscribers: [path.join(__dirname, '../**/*.subscriber{.ts,.js}')],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber',
-  },
-};
+export default class MysqlDBConnection {
+  private connectionInfo: ConnectionOptions = {
+    type: 'mysql',
+    host: host,
+    port: 3306,
+    username: user,
+    password: password,
+    database: database,
+    synchronize: true,
+    logging: ['error'],
+    entities: [path.join(__dirname, '../**/*.entity{.ts,.js}')],
+    migrations: [path.join(__dirname, '../**/*.migration{.ts,.js}')],
+    subscribers: [path.join(__dirname, '../**/*.subscriber{.ts,.js}')],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber',
+    },
+  };
 
-export default MysqlDBConnection;
+  constructor() {
+    createConnection(this.connectionInfo)
+      .then(() => {
+        global._logger.info('MysqlDB connect success');
+      })
+      .catch((error) => {
+        global._logger.error('MysqlDB connect error', error);
+      });
+  }
+}
