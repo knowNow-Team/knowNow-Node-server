70dd85d398dd30ab7d9a9bbc199b9b7a30eea013
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-28 20:56:29 +0900

    Etc: 불필요한 코드 삭제

--- src/controllers/v1/words.controller.ts
+++ src/controllers/v1/words.controller.ts
@@ -29,18 +29,6 @@ class WordController {
       next(err);
     }
   };
-
-  public updateWord = async (req: Request, res: Response, next: NextFunction) => {
-    const wordId: string = req.params.wordId;
-    const wordData: IWord = req.body; // 추후 토큰으로 받으면 유효성 검사해서 업데이트 할 것
-    try {
-      if (util.isEmpty(wordData)) throw new HttpException(statusCode.BAD_REQUEST, resMessage.NULL_VALUE);
-      const updateWordData: IWord = await this.WordService.updateWord(wordId, wordData);
-      return res.status(statusCode.OK).json({ message: resMessage.X_UPDATE_SUCCESS(WORD), data: updateWordData });
-    } catch (err) {
-      next(err);
-    }
-  };
 }
 
 export default WordController;
