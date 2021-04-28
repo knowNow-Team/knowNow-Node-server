8278fb39395ca9b866da265a4efb8d52f2472490
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-28 20:56:29 +0900

    Etc: 불필요한 코드 삭제

--- src/controllers/v1/words.controller.ts
+++ src/controllers/v1/words.controller.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { resMessage, statusCode, util } from '../../utils';
 import HttpException from '../../exceptions/HttpException';
-import { WordDto } from '../../dtos/words.dto';
 import { IWord } from '../../interfaces/words.interface';
 import WordService from '../../services/v1/words.service';
 
@@ -27,18 +26,6 @@ class WordController {
       if (util.isEmpty(wordData)) throw new HttpException(statusCode.BAD_REQUEST, resMessage.NULL_VALUE);
       const updateWordData: IWord = await this.WordService.updateWord(wordId, wordData);
       return res.status(statusCode.OK).json({ message: resMessage.X_UPDATE_SUCCESS(WORD), data: updateWordData });
-    } catch (err) {
-      next(err);
-    }
-  };
-
-  public deleteWord = async (req: Request, res: Response, next: NextFunction) => {
-    const wordId: string = req.params.wordId;
-    const { userId }: { userId: number } = req.body; // 추후 토큰으로 받으면 유효성 검사해서 불러올 것.
-
-    try {
-      const updateWordById = await this.WordService.deleteWordData(wordId);
-      return res.status(statusCode.OK).json({ message: resMessage.X_DELETE_SUCCESS(WORD), data: updateWordById });
     } catch (err) {
       next(err);
     }
