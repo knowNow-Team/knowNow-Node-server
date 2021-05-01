5634a0ea00c4baa2f55dde7bfe0e203f6a9544ec
YeongWoooo <rdd9223@gmail.com>
2021-05-01 19:16:14 +0900

    Feat: 단어 목록으로 단어 정보 찾기 구현

--- src/controllers/v1/words.controller.ts
+++ src/controllers/v1/words.controller.ts
@@ -3,6 +3,7 @@ import { resMessage, statusCode, util } from '../../utils';
 import HttpException from '../../exceptions/HttpException';
 import { IWord } from '../../interfaces/words.interface';
 import WordService from '../../services/v1/words.service';
+import { WordDto } from '../../dtos/words.dto';
 
 const WORD = '단어';
 
@@ -20,6 +21,29 @@ class WordController {
     }
   };
 
+  public getWordsByName = async (req: Request, res: Response, next: NextFunction) => {
+    const wordNames: string[] = req.body.wordNames;
+
+    try {
+      const wordInfos = await Promise.all(
+        wordNames.map(async (wordName: string) => {
+          const wordInfo = await this.WordService.findWordByName(wordName);
+          if (wordInfo) {
+            return wordInfo;
+          }
+          const scrappedWordInfo = await this.WordService.scrapWord(wordName);
+          const createdWordInfo = await this.WordService.createWord(JSON.parse(scrappedWordInfo));
+
+          return createdWordInfo;
+        }),
+      );
+
+      return res.status(statusCode.OK).json({ message: resMessage.X_READ_ALL_SUCCESS(WORD), data: wordInfos });
+    } catch (err) {
+      next(err);
+    }
+  };
+
   public createWord = async (req: Request, res: Response, next: NextFunction) => {
     const wordInfo: WordDto = req.body;
 
@@ -29,6 +53,19 @@ class WordController {
       const createdWordData: IWord = await this.WordService.createWord(wordInfo);
 
       return res.status(statusCode.CREATED).json({ message: resMessage.X_CREATE_SUCCESS, data: createdWordData });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public updateWord = async (req: Request, res: Response, next: NextFunction) => {
+    const wordId: string = req.params.wordId;
+    const wordData: IWord = req.body; // 추후 토큰으로 받으면 유효성 검사해서 업데이트 할 것
+
+    try {
+      if (util.isEmpty(wordData)) throw new HttpException(statusCode.BAD_REQUEST, resMessage.NULL_VALUE);
+      const updateWordData: IWord = await this.WordService.updateWord(wordId, wordData);
+      return res.status(statusCode.OK).json({ message: resMessage.X_UPDATE_SUCCESS(WORD), data: updateWordData });
     } catch (err) {
       next(err);
     }
