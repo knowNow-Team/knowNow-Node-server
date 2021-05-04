085ffa1f173b32f1f8ddc281e02fd10c6c787a06
god9599 <59276134+god9599@users.noreply.github.com>
2021-05-05 02:33:02 +0900

    Refactor: 응답 메시지 변경

--- src/controllers/v1/wordbooks.controller.ts
+++ src/controllers/v1/wordbooks.controller.ts
@@ -104,7 +104,6 @@ class WordbookController {
     const wordId: string = req.params.wordId;
     const wordbookId: string = req.params.wordbookId;
     const { userId }: { userId: number } = req.body; // 추후 토큰으로 받으면 유효성 검사해서 불러올 것.
-
     try {
       const updateWordById = await this.WordbookService.deleteWordData(wordbookId, wordId, userId);
       return res.status(statusCode.OK).json({ message: resMessage.X_DELETE_SUCCESS(WORD), data: updateWordById });
@@ -128,7 +127,7 @@ class WordbookController {
     const { userId }: { userId: number } = req.body; // 추후 토큰으로 받으면 유효성 검사해서 불러올 것.
     try {
       const removeWordData = await this.WordbookService.removeWordData(wordId, userId);
-      return res.status(statusCode.OK).json({ message: resMessage.X_DELETE_SUCCESS(WORDBOOK), data: removeWordData });
+      return res.status(statusCode.OK).json({ message: resMessage.X_DELETE_SUCCESS(WORD), data: removeWordData });
     } catch (err) {
       next(err);
     }
