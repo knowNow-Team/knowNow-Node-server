cb5b42d64d40f765d8f64bc6da7c7881b514c29a
YeongWoooo <rdd9223@gmail.com>
2021-05-16 04:13:37 +0900

    Style: 사용하지 않는 코드 삭제

--- src/interfaces/auths.interface.ts
+++ src/interfaces/auths.interface.ts
@@ -1,15 +1,8 @@
-import { Request } from 'express';
-import { IUser } from './users.interface';
-
 export interface DataStoredInToken {
-  _id: string;
+  id: number;
 }
 
 export interface TokenData {
   token: string;
   expiresIn: number;
-}
-
-export interface RequestWithUser extends Request {
-  user: IUser;
 }
