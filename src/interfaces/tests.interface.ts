9b61e4067529faa5724d99c277a9f54e630ca814
god9599 <59276134+god9599@users.noreply.github.com>
2021-05-15 18:45:51 +0900

    Refactor: status -> filter로 워딩 통일

--- src/interfaces/tests.interface.ts
+++ src/interfaces/tests.interface.ts
@@ -4,7 +4,7 @@ export interface IWord {
   answer?: string; // 제출한 정답
 }
 
-export enum ETestStatus {
+export enum ETestFilter {
   memorized = 'memorized', // 외웠어요
   confused = 'confused', // 헷갈려요
   doNotKnow = 'doNotKnow', // 몰라요
@@ -15,7 +15,7 @@ export interface ITest {
   difficulty: string; // 난이도
   wordTotalCount: number; // 단어 전체 수
   correctAnswerCount: number; // 정답 수
-  status: ETestStatus[]; // 몰라요, 헷갈려요, 다외웠어요 타입
+  filter: ETestFilter[]; // 몰라요, 헷갈려요, 다외웠어요 타입
   words: IWord[]; // 영어단어 리스트
   wordbooks: string[]; // 영어단어장 리스트
   score: number; // 점수
