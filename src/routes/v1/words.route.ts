f0644f1642d58691556da4b3a7a25f51d8a1ff41
YeongWoooo <rdd9223@gmail.com>
2021-05-18 01:18:51 +0900

    Refactor: 의미에 맞는 라우트 명 변경

--- src/routes/v1/words.route.ts
+++ src/routes/v1/words.route.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import IRoute from '@interfaces/routes.interface';
-import validationMiddleware from '@middlewares/validation.middleware';
-import authMiddleware from '../../middlewares/auth.middleware';
 import WordController from '@controllers/v1/words.controller';
 import { WordDto } from '@dtos/words.dto';
+import IRoute from '@interfaces/routes.interface';
+import validationMiddleware from '@middlewares/validation.middleware';
+import { Router } from 'express';
+import authMiddleware from '../../middlewares/auth.middleware';
 
 class WordRoute implements IRoute {
   public path = '/v1/words';
@@ -22,7 +22,7 @@ class WordRoute implements IRoute {
       validationMiddleware(WordDto, 'body', true),
       this.wordsController.createWord,
     );
-    this.router.post(`${this.path}/list`, authMiddleware, this.wordsController.getWordsByName);
+    this.router.post(`${this.path}/scrap`, authMiddleware, this.wordsController.getWordsByName);
     this.router.put(`${this.path}/:wordId`, authMiddleware, this.wordsController.updateWord);
   }
 }
